b54b724b4bfd0e9483f22b5dd36c311614b0d6ed
Gabriel Hernandez <gabrieldejesushernandez@gmail.com>
2021-11-18 06:03:39 +0000

    Ask for the user choice and assign the output to a variable that will be rock, paper or scissors. I also do the same with the computer choice, but this one is random with randint().

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -26,4 +28,26 @@ scissors = '''
 '''
 
 #Write your code below this line 👇
+computer_choice = random.randint(0,2)
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors.\n"))
 
+if user_choice == 0:
+  user_choice = rock
+elif user_choice == 1:
+  user_choice = paper
+elif user_choice == 2:
+  user_choice = scissors
+else:
+  print("ERROR, please select a number from 0 to 2(0,1,2)")
+
+if computer_choice == 0:
+  computer_choice = rock
+elif computer_choice == 1:
+  computer_choice = paper
+else:
+  computer_choice = scissors
+
+print(user_choice)
+
+print("\n\n\nComputer chose: \n\n\n")
+print(computer_choice)
